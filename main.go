6d3c3420e7193874b2f3cd8cdf047228fb4ffa9e
tkpd-firmanmm <firman.m@tokopedia.com>
2022-08-01 11:44:15 +0700

    Stuff

--- main.go
+++ main.go
@@ -4,9 +4,20 @@ import "fmt"
 
 func main() {
 	RandomStuff()
+	RandomWackyStuff()
 }
 
 // RandomStuff comment
 func RandomStuff() {
 	fmt.Println("Print from RandomStuff")
+}
+
+// RandomWackyStuffV2 comment.
+func RandomWackyStuffV2() {
+	fmt.Println("Print from RandomWackyStuff")
+}
+
+// RandomWackyStuff comment
+func RandomWackyStuff() {
+	fmt.Println("Print from RandomWackyStuff")
 }
